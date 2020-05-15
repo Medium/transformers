acd0dbefab96ef69dc65f1888bb080836931baaa
Deniz <dzorlu@medium.com>
2020-05-15 11:18:33 -0700

    reset vocav size for the embedding mod

--- src/transformers/modeling_tf_t5.py
+++ src/transformers/modeling_tf_t5.py
@@ -982,6 +982,7 @@ class TFT5ForConditionalGeneration(TFT5PreTrainedModel):
 
     def set_input_embeddings(self, new_embeddings):
         self.shared.weight = new_embeddings
+        self.shared.vocab_size = new_embeddings.shape[0]
         # retrieve correct absolute scope for embed token wrapper
         with tf.compat.v1.variable_scope("shared") as shared_abs_scope_name:
             pass
